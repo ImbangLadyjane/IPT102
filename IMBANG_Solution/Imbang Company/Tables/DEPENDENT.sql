6e37fb9ec1839a9a5343e23a5461379ef3628450
ImbangLadyjane <125239663+ImbangLadyjane@users.noreply.github.com>
2024-09-10 23:09:03 +0800

    Add files via upload

---
+++ IMBANG_Solution/Imbang Company/Tables/DEPENDENT.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DEPENDENT]
+(
+	[Essn] CHAR(9) NOT NULL PRIMARY KEY, 
+    [Dependent_name] VARCHAR(15) NOT NULL, 
+    [Sex] CHAR(9) NULL, 
+    [Bdate] DATE NULL, 
+    [Relationship] VARCHAR(8) NULL
+    Constraint [PK_Dependent_Name] Primary key (Essn, Dependent_name),
+    Constraint [FK_Dependent_Ssn] Foreign key (Essn) References Employee(Ssn)
+);
+GO
+    create nonclustered Index [Ix_Dependent_Sex] ON [Dependent]([Sex] ASC)
