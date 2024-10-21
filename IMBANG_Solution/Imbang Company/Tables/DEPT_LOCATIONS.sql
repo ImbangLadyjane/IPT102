c429e925559ab6d41e2cd8e4448185066093bfc3
ImbangLadyjane <125239663+ImbangLadyjane@users.noreply.github.com>
2024-10-21 01:18:11 -0700

    Delete IMBANG_Solution directory

--- IMBANG_Solution/Imbang Company/Tables/DEPT_LOCATIONS.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[DEPT_LOCATIONS]
-(
-	[Dnumber] INT NOT NULL Constraint [PK_Dept.Location_Locaton] Primary key,
-    [Dlocation] VARCHAR(15) NOT NULL Constraint [FK_Dept.Location_Number] Foreign Key (Dnumber) References Department(Dnumber)
-);
-GO
-
-	create  nonclustered index [IX_Dept.Location_Locstion] On [DEPT_LOCATIONS] ([Dlocation] ASC)
